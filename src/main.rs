064f144234fd18aedaab0ca7736573f68a92da40
johnson <zhangsen10@cnhqd.net>
2020-09-27 16:58:20 +0800

    using loop to guessing number

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,30 @@ fn main() {
     println!("Guessing the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
-    println!("pls input your guess.");
+    loop{
+        println!("pls input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("pls type a number");
-
-    println!("your guesses: {}", guess);
-
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+    
+        let guess: u32 = guess.trim().parse().expect("pls type a number");
+    
+        println!("your guesses: {}", guess);
+    
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+    
 }
